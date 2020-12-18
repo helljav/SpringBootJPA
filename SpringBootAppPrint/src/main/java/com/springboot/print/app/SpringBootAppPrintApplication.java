da539cba5efa5e028f7dadf641193a0ed60a1d37
Helljav <miromancequimico_004@hotmail.com>
2020-12-17 23:37:57 -0600

    Primer Comit
    Iniciando el proyecto

---
+++ SpringBootAppPrint/src/main/java/com/springboot/print/app/SpringBootAppPrintApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.print.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppPrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppPrintApplication.class, args);
+	}
+
+}
---
+++ SpringBootAppPrint/src/test/java/com/springboot/print/app/SpringBootAppPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.print.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppPrintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
